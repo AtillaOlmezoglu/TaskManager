8ce19f0c871c006878d5fa9f289b2ed09de8e3a3
AtillaOlmezoglu <atiolm@gmail.com>
2023-05-30 11:37:59 +0200

    Add project files.

---
+++ Data/TaskManagerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Models.Entities;
+
+namespace TaskManagerAPI.Data;
+
+public class TaskManagerContext : DbContext
+{
+    public DbSet<UserTask> Tasks { get; set; }
+
+    public TaskManagerContext(DbContextOptions<TaskManagerContext> options)
+        : base(options)
+    { }
+}
